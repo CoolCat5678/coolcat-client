5ef7c5f172c63d1ca1ccb8c8e46c0504e9bc4fc0
leochen <leochen@acube.com.tw>
2025-04-25 16:56:42 +0800

    feat: update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,39 +1,60 @@
 <template>
-  <v-footer
-    app
-    height="40"
-  >
-    <a
-      v-for="item in items"
-      :key="item.title"
-      class="d-inline-block mx-2 social-link"
-      :href="item.href"
-      rel="noopener noreferrer"
-      target="_blank"
-      :title="item.title"
-    >
-      <v-icon
-        :icon="item.icon"
-        :size="item.icon === '$vuetify' ? 24 : 16"
-      />
-    </a>
-
-    <div
-      class="text-caption text-disabled"
-      style="position: absolute; right: 16px;"
-    >
-      &copy; 2025-{{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">CoolCat5678</span>
-      — Designed and developed with
-      <a
-        class="text-decoration-none on-surface"
-        href="https://vuetifyjs.com/about/licensing/"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Vuetify
-      </a>
-    </div>
-  </v-footer>
+  <div>
+    <v-container class="border-t">
+      <v-row dense>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <div class="flex items-center justify-center h-full">
+            <a
+              v-for="item in items"
+              :key="item.title"
+              class="d-inline-block mx-2 social-link"
+              :href="item.href"
+              rel="noopener noreferrer"
+              target="_blank"
+              :title="item.title"
+            >
+              <v-icon
+                :icon="item.icon"
+                :size="24"
+              />
+            </a>
+          </div>
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <div class="flex items-center justify-center h-hull">
+            <div>
+              <div class="text-caption text-disabled text-center">
+                &copy; 2025-{{ (new Date()).getFullYear() }}
+                <a
+                  class="text-caption text-decoration-none on-surface"
+                  href="https://github.com/CoolCat5678/"
+                  target="_blank"
+                >
+                  CoolCat5678
+                </a>
+              </div>
+              <div class="text-caption text-disabled text-center">
+                Designed and developed with
+                <a
+                  class="text-decoration-none on-surface"
+                  href="https://vuetifyjs.com/about/licensing/"
+                  target="_blank"
+                >
+                  Vuetify
+                </a>
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script
@@ -44,6 +65,26 @@
     {
       title: 'CoolCat5678 GitHub',
       icon: `mdi-github`,
+      href: 'https://github.com/CoolCat5678',
+    },
+    {
+      title: 'CoolCat5678 GitHub',
+      icon: `mdi-linkedin`,
+      href: 'https://github.com/CoolCat5678',
+    },
+    {
+      title: 'CoolCat5678 GitHub',
+      icon: `mdi-facebook`,
+      href: 'https://github.com/CoolCat5678',
+    },
+    {
+      title: 'CoolCat5678 GitHub',
+      icon: `mdi-twitter`,
+      href: 'https://github.com/CoolCat5678',
+    },
+    {
+      title: 'CoolCat5678 GitHub',
+      icon: `mdi-google`,
       href: 'https://github.com/CoolCat5678',
     },
   ]
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,11 +5,10 @@
         <v-img
           class="mb-4"
           height="150"
-          src="@/assets/logo.png"
+          src="@/assets/coolcat.png"
         />
         <div class="mb-8 text-center">
-          <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-          <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+          <div class="text-h5 font-weight-bold">CoolCat</div>
         </div>
         <toggle-theme-btn />
       </v-col>
