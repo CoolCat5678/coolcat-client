54ebe8191ae04dd5372b6566eafb7fbf61bfb5e5
leochen <leochen@acube.com.tw>
2025-05-19 11:48:36 +0800

    feat: add vue3marquee

--- src/pages/home/components/SectionFour.vue
+++ src/pages/home/components/SectionFour.vue
@@ -1,46 +1,38 @@
 <template>
   <section
     ref="sectionTwoRef"
-    class="section-two-container"
+    class="section-four-container"
   >
-    <div
-      ref="sectionTwoCardRef"
-      class="section-two-card-wrapper"
+    <Vue3Marquee
+      class="h-full marquee-container"
+      :pause-on-hover="true"
     >
-      <div
-        class="section-two-tv-card"
-        elevation="6"
-        variant="outlined"
+      <img
+        alt="TV Wall"
+        class="tv-wall-img object-cover rounded-2xl"
+        src="@/assets/img/cinderella.jpg"
       >
-        <div class="tv-wall-grid">
-          <img
-            alt="TV Wall"
-            class="tv-wall-img object-cover rounded-2xl"
-            src="@/assets/img/cinderella.jpg"
-          >
-          <img
-            alt="TV Wall"
-            class="tv-wall-img object-cover rounded-2xl"
-            src="@/assets/img/doro.webp"
-          >
-          <img
-            alt="TV Wall"
-            class="tv-wall-img object-cover rounded-2xl"
-            src="@/assets/img/doro2.webp"
-          >
-          <img
-            alt="TV Wall"
-            class="tv-wall-img object-cover rounded-2xl"
-            src="@/assets/img/doroqq.jpg"
-          >
-          <img
-            alt="TV Wall"
-            class="tv-wall-img object-cover rounded-2xl"
-            src="@/assets/img/QQ.jpg"
-          >
-        </div>
-      </div>
-    </div>
+      <img
+        alt="TV Wall"
+        class="tv-wall-img object-cover rounded-2xl"
+        src="@/assets/img/doro.webp"
+      >
+      <img
+        alt="TV Wall"
+        class="tv-wall-img object-cover rounded-2xl"
+        src="@/assets/img/doro2.webp"
+      >
+      <img
+        alt="TV Wall"
+        class="tv-wall-img object-cover rounded-2xl"
+        src="@/assets/img/doroqq.jpg"
+      >
+      <img
+        alt="TV Wall"
+        class="tv-wall-img object-cover rounded-2xl"
+        src="@/assets/img/QQ.jpg"
+      >
+    </Vue3Marquee>
   </section>
 </template>
 
@@ -48,85 +40,21 @@
   setup
   lang="ts"
 >
-  import { onMounted, ref } from 'vue'
-  import gsap from 'gsap'
-  import ScrollTrigger from 'gsap/ScrollTrigger'
+  import { Vue3Marquee } from 'vue3-marquee'
 
-  gsap.registerPlugin(ScrollTrigger)
-
-  const sectionTwoRef = ref<HTMLElement | null>(null)
-  const sectionTwoCardRef = ref<HTMLElement | null>(null)
-
-  onMounted(() => {
-    if (!sectionTwoRef.value || !sectionTwoCardRef.value) return
-
-    // 抓出三張圖片
-    const images = sectionTwoCardRef.value.querySelectorAll<HTMLImageElement>('.tv-wall-img')
-
-    // 初始為透明
-    images.forEach(img => {
-      img.style.opacity = '0'
-    })
-
-    // 動畫時間線
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: sectionTwoRef.value,
-        start: 'top 100%',
-        toggleActions: 'play none none reset',
-
-      },
-    })
-
-    tl.fromTo(images[0], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' })
-      .fromTo(images[1], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-      .fromTo(images[2], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-      .fromTo(images[3], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-      .fromTo(images[4], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-    // .fromTo(images[5], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-    // .fromTo(images[6], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-    // .fromTo(images[7], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-    // .fromTo(images[8], { opacity: 0, y: 30 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }, '+=0.1')
-  })
 </script>
 
 <style scoped>
-  .section-two-container {
-    min-height: 80vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .section-two-card-wrapper {
-    width: 100%;
-  }
-
-  .section-two-tv-card {
-    height: 80vh;
+  .section-four-container {
+    height: 40vh;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 2rem;
   }
 
-  .tv-wall-grid {
-    min-width: 100vw;
-    display: flex;
-    height: 100%;
-    gap: 1rem;
-    overflow: hidden;
-    overflow-x: scroll;
-
-    scrollbar-width: none;
-    /* Firefox */
-    -ms-overflow-style: none;
-    /* Internet Explorer 10+ */
-  }
-
-  .tv-wall-grid::-webkit-scrollbar {
-    display: none;
-    /* Chrome, Safari, Opera */
+  .marquee-container {
+    gap: 4rem;
   }
 
   .tv-wall-img {
